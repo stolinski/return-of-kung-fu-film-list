289a5c704009ea24573302dfb2da385ff93d55c9
Scott Tolinski <scott.tolinski@teamdetroit.com>
2015-01-27 14:33:55 -0500

    adds themoviedb api

--- kungfu.js
+++ kungfu.js
@@ -1,4 +1,31 @@
-// simple-todos.js
+var apikey = 'e14d30d8866462614fa0b5a19b45e26f';
+var baseUrl = 'http://api.themoviedb.org/';
+var movieSearchUrl = baseUrl + '3/search/movie?api_key=' + apikey;
+var movieLookUp = baseUrl + '3/movie/';
+var query = "Human Lanterns";
+var year = "1982";
+var url = movieSearchUrl + "&query=" + query + "&year=" + year;
+
+// http://api.themoviedb.org/3/search/movie?api_key=e14d30d8866462614fa0b5a19b45e26f&query=Human Lanterns&year=1982
+
+
+if (Meteor.isServer) {
+    Meteor.methods({
+        searchMovies: function (title, year) {
+            url = movieSearchUrl + "&query=" + title + "&year=" + year;
+            console.log(url);
+            this.unblock();
+            return Meteor.http.call("GET", url);
+        },
+        detailMovie: function (id) {
+            url = movieLookUp + id + '?api_key=' + apikey;
+            console.log(url);
+            this.unblock();
+            return Meteor.http.call("GET", url);
+        }
+    });
+}
+
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
@@ -7,10 +34,8 @@ if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function () {
       if (Session.get("hideCompleted")) {
-        // If hide completed is checked, filter tasks
         return Tasks.find({checked: {$ne: true}, owner: Meteor.userId()}, {sort: {createdAt: -1}});
       } else {
-        // Otherwise, return all of the tasks
         return Tasks.find({owner: Meteor.userId()}, {sort: {createdAt: -1}});
       }
     },
@@ -32,13 +57,27 @@ if (Meteor.isClient) {
       var name = event.target.name.value;
       var rating = event.target.rating.value;
       var studio = event.target.studio.value;
+      var poster = '';
+      var backdrop = '';
+      var year = event.target.year.value;
 
-      Tasks.insert({
-        name: name,
-        owner: Meteor.userId(),
-        rating: rating,
-        studio: studio,
-        createdAt: new Date() // current time
+      Meteor.call("searchMovies", name, year, function(error, results) {
+        Meteor.call("detailMovie", results.data.results[0].id, function(error, results) {
+
+          poster = 'http://cf2.imgobject.com/t/p/w780' + results.data.poster_path;
+          backdrop = 'http://cf2.imgobject.com/t/p/w780' + results.data.backdrop_path;
+
+          Tasks.insert({
+            name: name,
+            owner: Meteor.userId(),
+            rating: rating,
+            studio: studio,
+            poster: poster,
+            backdrop: backdrop,
+            createdAt: new Date()
+          });
+
+        });
       });
 
       // Clear form
@@ -49,22 +88,28 @@ if (Meteor.isClient) {
     },
     "change .hide-completed input": function (event) {
       Session.set("hideCompleted", event.target.checked);
+    },
+
+    'click #fetchButton' : function () {
+      Meteor.call("checkTwitter", function(error, results) {
+        console.log(results.data.results[0]);
+      });
     }
   });
 
-Template.task.events({
-  "click .toggle-checked": function () {
-    // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {$set: {checked: ! this.checked}});
-  },
-  "click .delete": function () {
-    Tasks.remove(this._id);
-  }
-});
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
 
-// At the bottom of the client code
-Accounts.ui.config({
-  passwordSignupFields: "USERNAME_ONLY"
-});
+  // At the bottom of the client code
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 
 }
