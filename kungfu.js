4cfdc95c651675e73f77008647e2b393d0636cfe
Scott Tolinski <scott.tolinski@teamdetroit.com>
2015-01-27 16:37:35 -0500

    adds modal opening

--- kungfu.js
+++ kungfu.js
@@ -1,3 +1,4 @@
+
 var apikey = 'e14d30d8866462614fa0b5a19b45e26f';
 var baseUrl = 'http://api.themoviedb.org/';
 var movieSearchUrl = baseUrl + '3/search/movie?api_key=' + apikey;
@@ -30,30 +31,41 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
 
+  Meteor.startup(function(){
+      Session.set("sort_by", {createdAt: 1});
+      Session.set("detailModal", false);
+      Session.set("modalMovie", '5 deadly venoms');
+  });
 
   Template.body.helpers({
     tasks: function () {
-      if (Session.get("hideCompleted")) {
-        return Tasks.find({checked: {$ne: true}, owner: Meteor.userId()}, {sort: {createdAt: -1}});
-      } else {
-        return Tasks.find({owner: Meteor.userId()}, {sort: {createdAt: -1}});
-      }
+      return Tasks.find({owner: Meteor.userId()}, {sort: Session.get("sort_by")} );
+    },
+    details: function() {
+      return Tasks.findOne({name: Session.get("modalMovie")});
     },
     hideCompleted: function () {
       return Session.get("hideCompleted");
+    },
+    detailModal: function () {
+      return Session.get("detailModal");
     },
     incompleteCount: function () {
       return Tasks.find({checked: {$ne: true}, owner: Meteor.userId()}).count();
     },
     isAdmin: function() {
-      return (Meteor.user().username === 'admin');
+      if(Meteor.user()){
+        return (Meteor.user().username === 'admin');
+      } else {
+        return false;
+      }
+
     }
   });
 
   Template.body.events({
     "submit .new-task": function (event) {
 
-      console.log(event.target);
       var name = event.target.name.value;
       var rating = event.target.rating.value;
       var studio = event.target.studio.value;
@@ -64,16 +76,16 @@ if (Meteor.isClient) {
       Meteor.call("searchMovies", name, year, function(error, results) {
         Meteor.call("detailMovie", results.data.results[0].id, function(error, results) {
 
-          poster = 'http://cf2.imgobject.com/t/p/w780' + results.data.poster_path;
-          backdrop = 'http://cf2.imgobject.com/t/p/w780' + results.data.backdrop_path;
-
           Tasks.insert({
             name: name,
             owner: Meteor.userId(),
             rating: rating,
             studio: studio,
-            poster: poster,
-            backdrop: backdrop,
+            overview: results.data.overview,
+            release_date: results.data.release_date,
+            runtime: results.data.runtime,
+            poster: 'http://cf2.imgobject.com/t/p/w780' + results.data.poster_path,
+            backdrop: 'http://cf2.imgobject.com/t/p/w780' + results.data.backdrop_path,
             createdAt: new Date()
           });
 
@@ -86,14 +98,25 @@ if (Meteor.isClient) {
       // Prevent default form submit
       return false;
     },
+
     "change .hide-completed input": function (event) {
       Session.set("hideCompleted", event.target.checked);
     },
 
-    'click #fetchButton' : function () {
-      Meteor.call("checkTwitter", function(error, results) {
-        console.log(results.data.results[0]);
-      });
+    'click .film-listing' : function () {
+      Session.set("detailModal", true);
+    },
+
+    'click .detail-modal-close' : function () {
+      Session.set("detailModal", false);
+    },
+
+    "click #toggle": function() {
+      if( Session.get("sort_by").createdAt === 1) {
+        Session.set("sort_by", {createdAt: -1});
+      } else {
+        Session.set("sort_by", {createdAt: 1});
+      }
     }
   });
 
@@ -102,6 +125,7 @@ if (Meteor.isClient) {
       // Set the checked property to the opposite of its current value
       Tasks.update(this._id, {$set: {checked: ! this.checked}});
     },
+
     "click .delete": function () {
       Tasks.remove(this._id);
     }
